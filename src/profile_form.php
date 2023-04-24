553898094c8cc3c04fe93d117f44a691edae6aad
Hugues <hugues.farthouat@cpe.fr>
2023-04-24 09:28:08 +0200

    re

--- src/profile_form.php
+++ src/profile_form.php
@@ -6,7 +6,7 @@ session_start();
 
 $valid_session = is_valid_session();
 if($valid_session){
-    if(is_valid_user($_SESSION['user']['email'], $_SESSION['user']['password'])){
+    if(is_valid_user($_SESSION['user']['email'], $_POST['current_password'])){
         $sql_create_user = "UPDATE users set password = :password,name = :name ,last_name = :last_name where email = :email";
         $sql_args = array('email' => $_POST['email'] , 'password' => $_POST['password'], 'name' => $_POST['name'], 'last_name' => $_POST['last_name']);
         $result = requestSQL($sql_create_user, $sql_args);

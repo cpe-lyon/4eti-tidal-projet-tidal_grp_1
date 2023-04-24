b09d950075d1128adc4947153d6558b7559d19d9
Hugues <hugues.farthouat@cpe.fr>
2023-04-24 09:27:06 +0200

    re

--- src/profile_form.php
+++ src/profile_form.php
@@ -1,11 +1,23 @@
 <?php 
 require_once('./script.php');
+require_once('./login_utils.php');
+
 session_start();
 
-$sql_create_user = "UPDATE users set password = :password,name = :name ,last_name = :last_name where email = :email";
-$sql_args = array('email' => $_POST['email'] , 'password' => $_POST['password'], 'name' => $_POST['name'], 'last_name' => $_POST['last_name']);
-$result = requestSQL($sql_create_user, $sql_args);
+$valid_session = is_valid_session();
+if($valid_session){
+    if(is_valid_user($_SESSION['user']['email'], $_SESSION['user']['password'])){
+        $sql_create_user = "UPDATE users set password = :password,name = :name ,last_name = :last_name where email = :email";
+        $sql_args = array('email' => $_POST['email'] , 'password' => $_POST['password'], 'name' => $_POST['name'], 'last_name' => $_POST['last_name']);
+        $result = requestSQL($sql_create_user, $sql_args);
 
-$_SESSION['user'][0] = $sql_args;
-header('Location: /?page=profile');
+        $_SESSION['user'][0] = $sql_args;
+        header('Location: /?page=profile');
+    }else{
+        header('Location: /?page=profile_form');
+    }
+}else{
+    header('Location: /?page=profile_form');
+}
+
 ?>
